168130b61a1c31fdffd4ef5dcad3c5883e181cf4
rsandhumcr <rsandhumcr@gmail.com>
2024-08-11 21:32:26 +0100

    Add check for all resultsets data.

--- source_code/database_operations.py
+++ source_code/database_operations.py
@@ -218,11 +218,13 @@ class DatabaseOperations:
                 result_set = cursor.fetchall()
                 data_first = self.extract_result_data(result_set, cursor)
                 result.append(data_first)
-                while cursor.nextset():
+
+            while cursor.nextset():
+                if cursor.description is not None:
                     result_set = cursor.fetchall()
                     data_second = self.extract_result_data(result_set, cursor)
                     result.append(data_second)
-                    
+
             raw_connection.commit()
             raw_connection.close()
             return result
