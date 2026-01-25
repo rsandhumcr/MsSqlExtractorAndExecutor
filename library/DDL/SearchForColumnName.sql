16190babba355ac9effe7cdc03bd0ea019f94c75
rsandhumcr <rsandhumcr@gmail.com>
2026-01-25 17:09:55 +0000

    Move scripts.

--- library/DDL/SearchForColumnName.sql
+++ library/DDL/SearchForColumnName.sql
@@ -1,5 +1,5 @@
 --- parameters start
-DECLARE @search_term VARCHAR(100) = <value>;--- How many column name are you searching for ?
+DECLARE @search_term VARCHAR(100) = <value>;--- What column name are you searching for ?
 --- parameters end
 --- with results columns
 DECLARE @search_word VARCHAR(102) = '%' + @search_term + '%';
