7134d09261cec5022339ea2ddcb9a3ee540c3837
rsandhumcr <rsandhumcr@gmail.com>
2026-07-10 18:32:42 +0100

    Add cmd line option.

--- library_executor.py
+++ library_executor.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import datetime
 from source_code.database_operations import DatabaseOperations
 from source_code.file_operations import FileOperations
@@ -53,7 +54,7 @@ def execute_command_selection() -> None:
                 databaseSelector.execute_sql_script_no_data(db_config, script_data['sql_script'])
 
 
-def execute_script_with_result(db_config: dict[str, str|URL], script_data: dict[str, str], selected_file) -> None:
+def execute_script_with_result(db_config: dict[str, str|URL], script_data: dict[str, str], selected_file : str, specific_output_file:str|None) -> None:
     windows_end_line = True
     data_rows = databaseSelector.execute_sql_script_raw_connection(db_config, script_data['sql_script'])
     print_and_write_to_file(output_file, f"Db : {db_config['db_name']}\r\n", windows_end_line)
@@ -86,6 +87,7 @@ def execute_script_with_result(db_config: dict[str, str|URL], script_data: dict[
             result_set_count += 1
             if no_of_result_sets > 1:
                 print_and_write_to_file(output_file, f"Result set {result_set_count}\r\n", windows_end_line)
+
             if is_columns == 'Columns':
                 data_output_str = SqlOperations.show_table_result_columns(result_set_count, selected_file,
                                                                    data_row, show_headers)
@@ -94,6 +96,9 @@ def execute_script_with_result(db_config: dict[str, str|URL], script_data: dict[
 
             if is_columns == 'CSV':
                 data_output_str = SqlOperations.show_table_result_csv(data_row)
+
+            if data_output_str is not None:
+                print_and_write_to_file(specific_output_file, data_output_str, windows_end_line)
 
             current_time = get_current_timestamp()
             output_data = f'Start {current_time} \r\n{data_output_str} \r\nEnd {current_time}'
@@ -108,6 +113,32 @@ def print_and_write_to_file(file_name: str, data_output_str: str, make_windows_e
         data_output_str = data_output_str.replace('\r\n', '\n')
     file_operations.write_to_file(file_name, data_output_str)
 
+def execute_commandline() -> None:
+    args = sys.argv
+
+    db_name = args[1]
+    db_config = user_options.get_database_config_via_name(db_name)
+    if db_config == None:
+        print("There was an issue with the database configuration. Please check the configuration file and try again.")
+
+    new_path = args[2]
+    selected_file = args[3]
+
+    specific_output_file = None
+    if len(args) > 4:
+        specific_output_file = args[4]
+        specific_output_file = f".\\output\\{specific_output_file}"
+
+    full_path = f".\\library\\{new_path}\\{selected_file}"
+    sql_script = file_operations.read_file(full_path)
+    script_data = parse_sql_parameters.replace_parameters_with_prompts(sql_script)
+    if script_data['return_results']:
+        execute_script_with_result(db_config, script_data, selected_file, specific_output_file)
+    else:
+        databaseSelector.execute_sql_script_no_data(db_config, script_data['sql_script'])
 
 if __name__ == '__main__':
-    execute_command_selection()
+    if len(sys.argv) == 1:
+        execute_command_selection()
+    else:
+        execute_commandline()
